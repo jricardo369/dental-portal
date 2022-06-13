65027e8560b3307ac0dc6bab80b16cf445de7b98
MiguelVJ <miguel.vaz.jim@hotmail.com>
2022-06-13 14:07:01 -0500

    sociedad en pacientes y usuarios

--- src/app/services/pacientes.service.ts
+++ src/app/services/pacientes.service.ts
@@ -7,14 +7,21 @@ import { Paciente } from "./../../model/paciente";
   providedIn: "root",
 })
 export class PacientesService {
-  constructor(private http: HttpClient) {}
+
+  sociedad;
+
+  constructor(private http: HttpClient) {
+
+    let user = JSON.parse(localStorage.getItem('objUsuario'));
+			this.sociedad = user.sociedad.sociedad;
+      
+  }
 
   obtenerPacientes(): Promise<Paciente[]> {
     return new Promise<Paciente[]>((resolve, reject) =>
       this.http
         .get(
-          API_URL + "pacientes",
-          // .get(API_URL + 'pacientes/' + sociedad,
+          API_URL + "pacientes/por-sociedad/" + this.sociedad,
           {
             withCredentials: true,
             observe: "response",
